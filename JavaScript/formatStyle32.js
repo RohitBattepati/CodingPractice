088c8fc3b87c94b00eed2e672925c0371d8e4e1c
RohitBattepati <rohitbattepati93@gmail.com>
2019-04-19 22:32:50 -0400

    More JS

--- JavaScript/formatStyle32.js
+++ JavaScript/formatStyle32.js
@@ -12,20 +12,78 @@
 22      21      4321    4321    7 8 9 10
 1       1       54321   54321   11 12 13 14 15
 
-*/
+
 
 function pattern1(){
+    
+    let result = "";
     for ( let i =1 ;i < 6; i++){
         
-        for ( let j = 1; j<i+1; j++){
-            console.log(j);
-            
+        for ( let j = 1; j<i+1; j++){  
+            result += j+" ";
         }
-        console.log("\n");
-    }
-    
-
-    
+        console.log(result);
+        result=""; 
+    } 
 }
 
-pattern1();
+
+
+function pattern2(){
+    
+    let result2 = "";
+    for ( let i =1 ;i < 6; i++){
+        for ( let j = 1; j<i+1; j++){  
+            result2 += i+" ";
+        }
+        
+        console.log(result2);
+        result2="";
+       
+    } 
+}
+
+
+
+function pattern3(){
+    result3="";
+    for(let i=1; i<6; i++){
+        for(let j=1; j<=i; j++){
+            result3 += "*";
+        }
+        console.log(result3);
+        result3="";
+    }
+}
+
+pattern3();
+
+
+function pattern4(){
+    
+    let result = "";
+    for ( let i =5 ;i > 1; i--){
+        
+        for ( let j = 1; j<i+1; j++){  
+            result += i+" ";
+        }
+        console.log(result);
+        result=""; 
+    } 
+}
+
+*/
+
+function pattern4(){
+    
+    let result = "";
+    for ( let i =5 ;i > 1; i--){
+        
+        for ( let j = 1; j<i; j++){  
+            result += i+" ";
+        }
+        console.log(result);
+        result=""; 
+    } 
+}
+pattern4();
